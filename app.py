888640d4009778c3dfc7396764c5034e3983ffd2
RichardWangBerkeley <richardwang1009@berkeley.edu>
2023-10-01 11:57:09 -0400

    added file support

--- app.py
+++ app.py
@@ -5,9 +5,11 @@ from streamlit_lottie import st_lottie
 import json
 import plotly.express as px
 
+
 def load_lottiefile(filepath: str):
     with open(filepath, "r") as f:
         return json.load(f)
+
 
 # Title and caption
 st.title('Graph Wizard')
@@ -17,11 +19,11 @@ st.caption('Upload an excel file and generate a graph!')
 with st.sidebar:
     # Header
     st.header('Graph Editor')
-
+    st.write('Currently only supporting .xlsx files')
     # File uploader
     xy_options = ['File not uploaded']
     plots_options = ['Scatter Plot', 'Line Chart', 'Histogram', 'Bar Chart']
-    file = st.file_uploader(label = 'label', label_visibility = 'collapsed')
+    file = st.file_uploader(label='label', label_visibility='collapsed')
     if file is not None:
         df_xl = pd.read_excel(file)
         xy_options = df_xl.columns
@@ -30,7 +32,7 @@ with st.sidebar:
         'Please select the variable for the x-axis',
         (xy_options)
     )
-    
+
     # Y axis
     y_axis = st.selectbox(
         'Please select the variable for the y-axis',
@@ -46,15 +48,14 @@ with st.sidebar:
 if file is not None:
     df_xl
     if (plot == 'Line Chart'):
-        fig = px.line(df_xl, x = x_axis, y = y_axis)
+        fig = px.line(df_xl, x=x_axis, y=y_axis)
     elif (plot == 'Histogram'):
-        fig = px.histogram(df_xl, x = x_axis)
+        fig = px.histogram(df_xl, x=x_axis)
     elif (plot == 'Bar Chart'):
-        fig = px.bar(df_xl, x = x_axis, y = y_axis)
+        fig = px.bar(df_xl, x=x_axis, y=y_axis)
     else:
-        fig = px.scatter(df_xl, x = x_axis, y = y_axis)
+        fig = px.scatter(df_xl, x=x_axis, y=y_axis)
     fig
 else:
     files_animation = load_lottiefile("lottie/files.json")
     st_lottie(files_animation)
-
